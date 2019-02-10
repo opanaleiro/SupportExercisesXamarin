ace56d7dfae949d7186c802e7adc98b95782ac32
Jose Panadero <opanaleiro@hotmail.com>
2019-02-10 18:32:00 +0100

    Ejercicio realizado

---
+++ Ejercicio6/Ejercicio6/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void rotarBoton()
+        {
+
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+           await boton1.RotateTo(360, 800, Easing.SinInOut);
+        }
+    }
+}
